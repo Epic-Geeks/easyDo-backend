3c611fa4efce55f965a25fb4b6b4b18e75a452fa
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-10-16 13:58:43 +0300

    fixed some naming bugs

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -1,63 +1,56 @@
-' use strict ';
+" use strict ";
 
-const bcrypt = require( 'bcrypt' );
-const base64 = require( 'base-64' );
+const bcrypt = require("bcrypt");
+const base64 = require("base-64");
 
-const  Customer  = require( '../models' ).customerModel;
+const Customer = require("../models").customerModel;
 
-
-const signin = async ( req, res ) => {
-
-    const basicHeader = req.headers.authorization.split( ' ' );
-    const encodedValue = basicHeader.pop();
-    const decodedValue = base64.decode( encodedValue );
-    const [ username, password ] = decodedValue.split( ':' );
-    const user = await Customer.findOne( { where: { username } } );
-    if ( user ) {
-        const isSame = await bcrypt.compare( password, user.password );
-        if ( isSame ) {
-            return res.status( 200 ).json( {
-                "user": {
-                    "username": user.username,
-                    "email": user.email,
-                    "role": user.role
-                },
-                "token": user.token
-            } );
-        } else {
-            return res.status( 401 ).send( 'You are not authorized' );
-        }
+const signin = async (req, res) => {
+  const basicHeader = req.headers.authorization.split(" ");
+  const encodedValue = basicHeader.pop();
+  const decodedValue = base64.decode(encodedValue);
+  const [username, password] = decodedValue.split(":");
+  const user = await Customer.findOne({ where: { username } });
+  if (user) {
+    const isSame = await bcrypt.compare(password, user.password);
+    if (isSame) {
+      return res.status(200).json({
+        user: {
+          username: user.username,
+          email: user.email,
+          role: user.role,
+        },
+        token: user.token,
+      });
     } else {
-        return res.status( 401 ).send( 'You are not authorized' );
+      return res.status(401).send("You are not authorized");
     }
+  } else {
+    return res.status(401).send("You are not authorized");
+  }
 };
 
-
-const signup = async ( req, res ) => {
-
-    const { username, email, password, role } = req.body;
-    const output = { username, email, role , 
-        password: await bcrypt.hash( password, 10 ) };
-    const user = await Customer.create( output );
-    if ( user ) {
-        res.status( 201 ).json( { 
-            "user": {
-                "username": user.username,
-                "email": user.email,
-                "role": user.role
-
-            },
-            "token": user.token
-        } );
-    }
-    else { 
-        res.status( 403 ).send( 'Invalid Signup' );
-    }
-    };
+const signup = async (req, res) => {
+  const { username, email, password, role } = req.body;
+  const output = {
+    username,
+    email,
+    role,
+    password: await bcrypt.hash(password, 10),
+  };
+  const user = await Customer.create(output);
+  if (user) {
+    res.status(201).json({
+      user: {
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+      token: user.token,
+    });
+  } else {
+    res.status(403).send("Invalid Signup");
+  }
+};
 
 module.exports = { signup, signin };
-
-    
-
-
-
