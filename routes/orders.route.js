ed3d1968306bb9c4b32bfd0b8e70c03965c16ce5
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-10-23 12:24:51 +0300

    created chat module, calculated the avarge rate for the service

--- routes/orders.route.js
+++ routes/orders.route.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const express = require("express");
-const { Order } = require("../models");
+const { Order, orderModel, serviceModel } = require("../models");
 // eslint-disable-next-line new-cap
 const orders = express.Router();
 const serverError = require("../error-handlers/500");
@@ -56,9 +56,21 @@ async function updateCondition(req, res) {
   try {
     let requestedOrder = await Order.updateOrderStatus(
       req.params.id,
-      req.params.condition
+    req.params.condition,
+     req.body.rateService,
     );
-    res.status(200).json(requestedOrder);
+
+    if (req.params.condition == "done") {
+      let id = req.params.id;
+      let order = await orderModel.findOne({where: {id}});
+      let service = await serviceModel.findOne({where:{id: order.serviceID}});
+      let doneOrders = await orderModel.findAll({where:{serviceID: service.id,status:"done"}});
+      let sum = 0, length = 0;
+      doneOrders.forEach(a=> {sum+=a.rateService, length+=1});
+      await service.update({averageRate:Math.floor((sum/length)*100)/100});
+      return res.status(200).json(requestedOrder);
+    }
+    return res.status(200).json(requestedOrder);
   } catch (error) {
     console.log(error);
     res.status(500).json({
